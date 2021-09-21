e13de926150cd8cfec63b05cac9425ca1aada07e
victor rosa <vicrosa25@gmail.com>
2021-09-21 11:28:35 +0200

    Updates

--- src/errors/OptimisticLockVersionMismatchError.ts
+++ src/errors/OptimisticLockVersionMismatchError.ts
@@ -1,9 +1,14 @@
+import { CustomError } from "./CustomError";
+
 export class OptimisticLockVersionMismatchError extends Error {
-  name = "OptimisticLockVersionMismatchError";
+  statusCode = 409;
 
   constructor(entity: string, expectedVersion: number, actualVersion: number) {
     super();
-    Reflect.setPrototypeOf(this, OptimisticLockVersionMismatchError.prototype);
+    Object.setPrototypeOf(this, OptimisticLockVersionMismatchError.prototype);
     this.message = `The optimistic lock on entity ${entity} failed, version ${expectedVersion} was expected, but is actually ${actualVersion}.`;
+  }
+  serializeError() {
+    return [{ message: this.message }];
   }
 }
